2ba79818e5907b8bda2f7701748cf9f149c536c2
emileond <sonarart@gmail.com>
2020-08-02 20:05:49 -0500

    Working with Parcel

---
+++ src/js/scripts.js
@@ -0,0 +1,41 @@
+// AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
+
+// SASS Styles
+import './../sass/styles.scss'
+
+// Navbar Functionality
+const navbar = document.querySelector('.navbar');
+const h1 = document.querySelector('h1');
+const mainNav = document.querySelector('.main-nav');
+const navBarToggle = document.querySelector('.navbar-toggle');
+const navItems = document.querySelectorAll('.main-nav li')
+
+// Mobile Toggle
+navBarToggle.addEventListener('click', () => { mainNav.classList.toggle('active') });
+
+// Navbar Menu Active Item
+function activeLink() {
+    mainNav.classList.remove('active');
+}
+
+navItems.forEach(el => {
+    el.addEventListener('click', activeLink)
+})
+
+// Scroll
+const scrollObs = new IntersectionObserver(scrollObserver);
+
+function scrollObserver(payload) {
+    if (!payload[0].isIntersecting) {
+        navbar.classList.add('scrolled')
+    }
+    else {
+        navbar.classList.remove('scrolled')
+    }
+}
+
+scrollObs.observe(h1);
